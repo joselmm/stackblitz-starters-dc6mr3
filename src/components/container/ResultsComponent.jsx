154b468e16042a719347c89aadb24315f2663c9d
joselmm <95531879+joselmm@users.noreply.github.com>
2023-06-12 16:02:49 -0500

    1 file modified

--- src/components/container/ResultsComponent.jsx
+++ src/components/container/ResultsComponent.jsx
@@ -6,14 +6,7 @@ import VideoCardComponent from '../pure/VideoCardComponent.jsx';
 const ResultComponent = ({ videos, setVideos, setPlaylist, playlist }) => {
   /* add to playlist */
   function toggleAddToPlaylist(videoInfo) {
-    //console.log(videos);
-    const tempVideolist = [...videos];
-    const index = tempVideolist.indexOf(videoInfo);
-    tempVideolist[index].addedToPlayList =
-      !tempVideolist[index].addedToPlayList;
-    console.log(tempVideolist[index]);
-    setVideos(tempVideolist);
-
+    
     switch (videoInfo.addedToPlaylist) {
       case true:
         {
@@ -26,10 +19,18 @@ const ResultComponent = ({ videos, setVideos, setPlaylist, playlist }) => {
         }
         break;
     }
+
+    //console.log(videos);
+    const tempVideolist = [...videos];
+    const index = tempVideolist.indexOf(videoInfo);
+    tempVideolist[index].addedToPlayList =
+      !tempVideolist[index].addedToPlayList;
+   // console.log(tempVideolist[index]);
+    setVideos(tempVideolist);
   }
 
   function removeFromPlaylist(videoId) {
-    //console.log(videos);
+    console.log("remove");
     const tempPlaylist = [...playlist];
     const index = null;
     var iteracion = 0;
@@ -47,6 +48,8 @@ const ResultComponent = ({ videos, setVideos, setPlaylist, playlist }) => {
 
   function addToPlaylist(info) {
     //console.log(videos);
+    console.log("add");
+
     const tempPlaylist = [...playlist];
     const playlistItem = new PlayListItem({
       title: info.title,
@@ -55,6 +58,8 @@ const ResultComponent = ({ videos, setVideos, setPlaylist, playlist }) => {
       videoId: info.videoId,
       state: PLAYLIST_ITEM_STATE.WAITING,
     });
+    tempPlaylist.push(playlistItem);
+    setPlaylist(tempPlaylist)
   }
 
   return (
